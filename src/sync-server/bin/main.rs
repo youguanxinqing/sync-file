8ffeec912d84d56a02299b09c51f70a858661c11
youguanxinqing <youguanxinqing@qq.com>
2024-04-20 15:46:46 +0800

    add write file logic

--- src/sync-server/bin/main.rs
+++ src/sync-server/bin/main.rs
@@ -1,7 +1,11 @@
 use actix_multipart::{Field, Multipart};
-use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder, Result};
+use actix_web::{
+    get, post, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder, Result,
+};
+use chrono::Local;
 use futures::StreamExt;
 use log::{debug, warn};
+use std::path;
 
 #[get("/ping")]
 async fn ping() -> Result<impl Responder> {
@@ -18,14 +22,14 @@ struct UploadForm {
 #[derive(Debug)]
 enum Action {
     Safe,
-    Fore,
+    Force,
 }
 
 impl Action {
     fn from_str(action: &str) -> Self {
         match action {
             "safe" => Action::Safe,
-            "fore" => Action::Fore,
+            "fore" => Action::Force,
             _ => Action::Safe,
         }
     }
@@ -42,7 +46,7 @@ async fn upload(req: HttpRequest, bytes: web::Payload) -> Result<impl Responder>
     let mut multipart = Multipart::new(req.headers(), bytes);
 
     // parse multipart
-    let mut forms = UploadForm::default();
+    let mut form = UploadForm::default();
     // let mut upload_file = fs::File::create(format!("1.txt")).unwrap();
     while let Some(chunk) = multipart.next().await {
         let mut chunk = chunk?;
@@ -52,13 +56,13 @@ async fn upload(req: HttpRequest, bytes: web::Payload) -> Result<impl Responder>
         let value = read_content_disposition(&mut chunk).await;
         match key {
             "action" => {
-                forms.action = Action::from_str(&value);
+                form.action = Action::from_str(&value);
             }
             "file" => {
-                forms.content = value;
+                form.content = value;
             }
             "target_file_path" => {
-                forms.target_file_path = value;
+                form.target_file_path = value;
             }
             _ => {
                 warn!("unknown action '{}'", key);
@@ -66,24 +70,97 @@ async fn upload(req: HttpRequest, bytes: web::Payload) -> Result<impl Responder>
         }
     }
 
-    if let Err(err) = validate_upload_args(&forms) {
-        return Ok(HttpResponse::BadRequest().body(err.to_string()));
+    if let Err(err) = validate_upload_args(&form) {
+        return Ok(HttpResponse::BadRequest().body(format!("validate form err: {}", err)));
     }
 
-    match forms.action {
-        Action::Safe => safe_write(&forms).await,
-        Action::Fore => force_write(&forms).await,
+    if let Err(err) = match form.action {
+        Action::Safe => safe_write(&form).await,
+        Action::Force => force_write(&form).await,
+    } {
+        return Ok(HttpResponse::BadRequest().body(format!("write file err: {}", err)));
     }
 
     Ok(HttpResponse::Ok().body(format!("Upload Successfully!")))
 }
 
-async fn safe_write(forms: &UploadForm) {}
+async fn safe_create_backup_dir(dir_path: &str) -> std::result::Result<(), String> {
+    let path_obj = path::Path::new(dir_path);
+    if path_obj.exists() && !path_obj.is_dir() {
+        return Err(format!("backup dir is conflicted: {}", dir_path));
+    }
 
-async fn force_write(forms: &UploadForm) {}
+    if !path_obj.exists() {
+        if let Err(err) = tokio::fs::create_dir_all(dir_path).await {
+            return Err(format!("create backup dir err: {}", err));
+        }
+    }
 
-fn validate_upload_args(forms: &UploadForm) -> Result<()> {
-    debug!("{:?}", forms);
+    Ok(())
+}
+
+async fn safe_write(form: &UploadForm) -> std::result::Result<(), String> {
+    let target_path = path::Path::new(&form.target_file_path);
+    let filename = target_path.file_name().unwrap().to_str().unwrap();
+    let filename_without_ext = path::Path::new(filename)
+        .file_stem()
+        .unwrap()
+        .to_str()
+        .unwrap_or(filename);
+    let dir = target_path.parent().unwrap().to_str().unwrap().to_string();
+
+    debug!(
+        "1111: {:?} {:?} {:?}",
+        &filename, &filename_without_ext, &dir
+    );
+
+    // create backup dir
+    let dot_backup_dir = format!("{}/.{}", dir, filename_without_ext);
+    safe_create_backup_dir(&dot_backup_dir).await?;
+
+    // backup file
+    if let Err(err) = tokio::fs::copy(
+        target_path,
+        format!(
+            "{}/{}.{}",
+            dot_backup_dir,
+            filename,
+            Local::now().format("%Y%m%d_%H%M%S")
+        ),
+    )
+    .await
+    {
+        return Err(format!("copy backup err: {}", err));
+    }
+
+    // TODO Check md5. Return directly if md5 does not change.
+
+    // truncate && write new content
+    if let Err(err) = tokio::fs::write(target_path, &form.content).await {
+        return Err(format!("write file err: {}", err));
+    }
+
+    Ok(())
+}
+
+async fn force_write(form: &UploadForm) -> std::result::Result<(), String> {
+    let target_path = path::Path::new(&form.target_file_path);
+    if let Err(err) = tokio::fs::write(target_path, &form.content).await {
+        return Err(format!("write file err: {}", err));
+    }
+    Ok(())
+}
+
+fn validate_upload_args(form: &UploadForm) -> std::result::Result<(), String> {
+    debug!("{:?}", form);
+
+    if form.target_file_path.is_empty() {
+        return Err("target_file_path is empty".to_string());
+    }
+    if form.target_file_path.split("/").collect::<String>().len() < 2 {
+        return Err("require level of dir is more than 2".to_string());
+    }
+
     Ok(())
 }
 
