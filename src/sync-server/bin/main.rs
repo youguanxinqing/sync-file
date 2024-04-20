f5a86bf6a0df5797bbdf758cfc424a7b5faaae4f
youguanxinqing <youguanxinqing@qq.com>
2024-04-20 19:30:58 +0800

    add clap, support command line

--- src/sync-server/bin/main.rs
+++ src/sync-server/bin/main.rs
@@ -2,8 +2,9 @@ use actix_multipart::{Field, Multipart};
 use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder, Result};
 use chrono::Local;
 use futures::StreamExt;
-use log::{debug, warn};
+use log::{debug, info, warn};
 use std::{path, str::FromStr};
+use clap::Parser;
 
 #[get("/ping")]
 async fn ping() -> Result<impl Responder> {
@@ -190,11 +191,24 @@ async fn read_content_disposition(chunk: &mut Field) -> String {
     return s;
 }
 
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(long, default_value_t = String::from("127.0.0.1"))]
+    host: String,
+    
+    #[arg(long, default_value_t = 9091)]
+    port: u16,
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init();
+    
+    let args = Args::parse();
+    info!("Start sync-server. Args={:?}", args);
+
     HttpServer::new(|| App::new().service(ping).service(upload))
-        .bind(("127.0.0.1", 8080))?
+        .bind((args.host, args.port))?
         .run()
         .await
 }
