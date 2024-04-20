7e6d4e202f6b6b27211b56eb6a502c9977354160
youguanxinqing <youguanxinqing@qq.com>
2024-04-20 12:40:50 +0800

    parse multipart

--- src/sync-server/bin/main.rs
+++ src/sync-server/bin/main.rs
@@ -1,43 +1,108 @@
-use actix_multipart::form::{tempfile::TempFile, MultipartForm};
-use actix_web::{cookie::time::format_description::well_known::iso8601::Config, get, post, web, App, HttpResponse, HttpServer, Responder, Result};
-use std::{cmp::PartialOrd, path::PathBuf};
-
+use actix_multipart::{Field, Multipart};
+use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder, Result};
+use futures::StreamExt;
+use log::{debug, warn};
 
 #[get("/ping")]
 async fn ping() -> Result<impl Responder> {
     Ok(HttpResponse::Ok().body("Ping OK!!!"))
 }
 
-#[derive(MultipartForm)]
-struct UploadRequest {
-    file: TempFile,
+#[derive(Debug, Default)]
+struct UploadForm {
+    action: Action,
+    content: String,
+    target_file_path: String,
+}
+
+#[derive(Debug)]
+enum Action {
+    Safe,
+    Fore,
+}
+
+impl Action {
+    fn from_str(action: &str) -> Self {
+        match action {
+            "safe" => Action::Safe,
+            "fore" => Action::Fore,
+            _ => Action::Safe,
+        }
+    }
+}
+
+impl Default for Action {
+    fn default() -> Self {
+        Action::Safe
+    }
 }
 
 #[post("/")]
-async fn upload(form: MultipartForm<UploadRequest>) -> Result<impl Responder> {
-    const MAX_FILE_SIZE: u64 = 1024 * 1024 * 10; // 10M
-    const MAX_FILE_COUNT: u32 = 1;
+async fn upload(req: HttpRequest, bytes: web::Payload) -> Result<impl Responder> {
+    let mut multipart = Multipart::new(req.headers(), bytes);
 
-    match form.file.size {
-        0 => return Ok(HttpResponse::BadRequest().finish()),
-        length if length > (MAX_FILE_SIZE as usize) => {
-            return Ok(HttpResponse::BadRequest()
-                .body(format!("The uploaded file is too large. Maximum size is {} bytes.", MAX_FILE_SIZE)));
-        },
-        _ => {}
+    // parse multipart
+    let mut forms = UploadForm::default();
+    // let mut upload_file = fs::File::create(format!("1.txt")).unwrap();
+    while let Some(chunk) = multipart.next().await {
+        let mut chunk = chunk?;
+
+        let content_disposition = chunk.content_disposition().clone();
+        let key = content_disposition.get_name().unwrap_or("");
+        let value = read_content_disposition(&mut chunk).await;
+        match key {
+            "action" => {
+                forms.action = Action::from_str(&value);
+            }
+            "file" => {
+                forms.content = value;
+            }
+            "target_file_path" => {
+                forms.target_file_path = value;
+            }
+            _ => {
+                warn!("unknown action '{}'", key);
+            }
+        }
     }
 
-    let temp_file = form.file.file.path();
-    let file_name = form.file.file_name.as_ref().map(|m| m.as_ref()).unwrap_or("null");
+    if let Err(err) = validate_upload_args(&forms) {
+        return Ok(HttpResponse::BadRequest().body(err.to_string()));
+    }
 
-    let path_file = PathBuf::from("1.txt");
+    match forms.action {
+        Action::Safe => safe_write(&forms).await,
+        Action::Fore => force_write(&forms).await,
+    }
 
-    Ok(HttpResponse::Ok().body("Upload Successfully!".to_string()))
+    Ok(HttpResponse::Ok().body(format!("Upload Successfully!")))
+}
+
+async fn safe_write(forms: &UploadForm) {}
+
+async fn force_write(forms: &UploadForm) {}
+
+fn validate_upload_args(forms: &UploadForm) -> Result<()> {
+    debug!("{:?}", forms);
+    Ok(())
+}
+
+async fn read_content_disposition(chunk: &mut Field) -> String {
+    let mut s = String::default();
+    while let Some(chunk_content) = chunk.next().await {
+        s.push_str(
+            String::from_utf8(chunk_content.unwrap_or_default().to_vec())
+                .unwrap_or_default()
+                .as_ref(),
+        );
+    }
+    return s;
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(ping))
+    env_logger::init();
+    HttpServer::new(|| App::new().service(ping).service(upload))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
