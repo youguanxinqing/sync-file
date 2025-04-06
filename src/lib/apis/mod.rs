8f14463df6a2046c0f0e02572557bf679941f4e5
youguanxinqing <youguanxinqing@qq.com>
2025-04-06 10:50:33 +0800

    optimize url by macro

--- src/lib/apis/mod.rs
+++ src/lib/apis/mod.rs
@@ -1,2 +1,20 @@
 pub mod ping;
 pub mod upload;
+
+pub mod urls {
+    #[macro_export]
+    macro_rules! __PING_URL_V1 {
+        ($protocol:expr, $addr:expr) => {
+            format!("{}://{}/ping", $protocol, $addr)
+        };
+    }
+    pub use __PING_URL_V1 as PING_URL_V1;
+
+    #[macro_export]
+    macro_rules! __UPLOAD_URL_V1 {
+        ($protocol:expr, $addr:expr) => {
+            format!("{}://{}/upload", $protocol, $addr)
+        };
+    }
+    pub use __UPLOAD_URL_V1 as UPLOAD_URL_V1;
+}
